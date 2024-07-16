be1de841e843e4ae0e103973b9b1d19dd17f403d
Sungho Kim <runtodream@gmail.com>
2024-07-16 22:32:29 +0900

    Update site

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# MkDocs 사이트 빌드
+mkdocs build
+
+# gh-pages 브랜치로 전환 (없으면 생성)
+git checkout gh-pages 
+
+# 이전 파일 삭제
+git rm -rf .
+
+# 빌드된 파일 복사
+cp -r site/* .
+
+# 파일 커밋 및 푸시
+git add .
+git commit -m "Update site"
+git push origin gh-pages --force
+
+# main 브랜치로 돌아가기
+git checkout master
+
